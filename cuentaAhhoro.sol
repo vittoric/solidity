cb92d5113dfdc37c21fed819287a2672a83ea0b8
toricode <93945847+vittoric@users.noreply.github.com>
2023-02-02 16:08:06 +0100

    Update cuentaAhhoro.sol

--- cuentaAhhoro.sol
+++ cuentaAhhoro.sol
@@ -3,6 +3,18 @@
 pragma solidity ^0.8.7;
 
 contract cuentaAhhoro{
+
+/*Actividad
+    Crea un smart comntract que sea capaz de gestionar una cuenta de ahorro.
+    Funciones clave que puede tener tu smart contract:
+    -Crear una persona que pueda guardar nombre, edad, saldo
+    -Asociar a la persona con su address
+    
+    -Agregar Saldo
+    -Quitar Saldo
+    -Ver Saldo
+    -Ver datos de la persona
+ */
     //Guardar m i nombre, edad, saldo
     //Guardar mi adress
 
