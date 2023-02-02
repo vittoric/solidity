7ed7b0aa23e227c666ad786acbc52403e6de1d39
toricode <93945847+vittoric@users.noreply.github.com>
2023-02-02 16:03:37 +0100

    Create cuentaAhhoro.sol

---
+++ cuentaAhhoro.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract cuentaAhhoro{
+    //Guardar m i nombre, edad, saldo
+    //Guardar mi adress
+
+    //Agregar saldo
+
+    //Quitar saldo
+
+    //VerSaldo
+
+}
