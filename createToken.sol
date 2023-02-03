fb65dbd3705a0b62a3b41873d7f2fe30c537c343
toricode <93945847+vittoric@users.noreply.github.com>
2023-02-03 12:53:11 +0100

    Update createToken.sol

--- createToken.sol
+++ createToken.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ToriCoin is ERC20 {
-    constructor() ERC20("ToriCoin", "TCR") {}
+    constructor(uint256 _initialSupply) ERC20("ToriCoin", "TCR") {
+        _mint(msg.sender, _initialSupply);
+    }
 }
